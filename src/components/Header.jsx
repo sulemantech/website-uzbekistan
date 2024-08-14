ebb0454cab23223ae4bf7e623f1fff26b30d9490
izaz ahmad <izazahmadhaider0@gmail.com>
2024-08-14 18:03:16 +0500

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,33 +2,32 @@ import React from "react";
 
 const Header = () => {
   return (
-    <>
-      <div className="">
-        <video
-          className="rounded-b-xl"
-          src="headerbgvideo.mov"
-          autoPlay
-          loop
-          muted
-          playsInline
-        ></video>
+    <div className="relative h-screen overflow-hidden">
+      <video
+        className="absolute rounded-3xl top-0 left-0 w-full h-full object-cover"
+        src="headerbgvideo.mov"
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
 
-        <span className="absolute top-5">
-          <img className="ml-5" src="gigafibertoplogo.svg" alt="" />
-        </span>
-        <div className="absolute bottom-5">
-          <p className="font-bold  text-8xl ml-5 text-white ">
-            TRANSFORMING INFRASTRUCTURE.
-          </p>
-          <p className="font-bold  text-8xl ml-5 text-[#1E1A2E] leading-2">
-            DRIVING REGIONAL GROWTH.​
-          </p>
-          <p className="font-bold  text-[20px] ml-5 text-white">
-            MEHR ERFAHREN →​
-          </p>
-        </div>
+      <span className="absolute top-5 left-5 z-10">
+        <img src="gigafibertoplogo.svg" alt="Gigafiber Logo" />
+      </span>
+
+      <div className="absolute bottom-5 left-5 z-10">
+        <p className="font-bold text-8xl text-white">
+          TRANSFORMING INFRASTRUCTURE.
+        </p>
+        <p className="font-bold text-8xl text-[#1E1A2E] leading-2">
+          DRIVING REGIONAL GROWTH.​
+        </p>
+        <p className="font-bold text-[20px] text-white mt-4">
+          MEHR ERFAHREN →​
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
