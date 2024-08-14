a879ec4e3f1e603d98d89ac19655171ec9e12bce
izaz ahmad <izazahmadhaider0@gmail.com>
2024-08-14 19:44:53 +0500

    ui changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 const Footer = ({ className = "" }) => {
   return (
     <div
-      className={`w-[94vw] mx-auto relative h-[15.5rem] text-left text-[1rem] text-white font-saira ${className}`}
+      className={`w-[97vw] mx-auto relative h-[15.5rem] text-left text-[1rem] text-white font-saira ${className}`}
     >
-      <div className="absolute top-[0rem] left-[0rem] rounded-22xl bg-giga-fiber-lila w-[94vw] mx-auto h-[15.5rem]" />
+      <div className="absolute top-[0rem] left-[0rem] rounded-22xl bg-giga-fiber-lila w-[97vw] mx-auto h-[15.5rem]" />
       <img
         className="absolute top-[calc(50%_+_46px)] left-[4.394rem] w-[18.75rem] h-[2.25rem] overflow-hidden"
         alt=""
