4bf8640ae0384f484e5bafeef9df62fcf1798d8b
izaz ahmad <izazahmadhaider0@gmail.com>
2024-08-15 12:29:49 +0500

    Changes

--- src/components/FrameComponent4.jsx
+++ src/components/FrameComponent4.jsx
@@ -28,20 +28,20 @@ const FrameComponent4 = () => {
   ];
 
   return (
-    <div className="flex justify-center flex-wrap mt-4 gap-3 p-2 ">
+    <div className="flex  justify-center flex-wrap mt-4 gap-3 p-2 ">
       {cardData.map((card, index) => (
         <div
           key={index}
-          className="relative cursor-pointer w-[445px] h-[582px] border border-gray-300 rounded-lg overflow-hidden"
+          className="relative group cursor-pointer w-[445px] h-[582px] max-lg:w-full max-lg:mx-4 rounded-lg overflow-hidden"
         >
           <p className="absolute top-[74px] left-[40px] text-5xl text-white z-50">{card.number}</p>
           <img
-            className="absolute top-0 left-0 w-full h-full object-cover"
+            className="absolute group-hover:scale-105 transition-all top-0 left-0 w-full h-full object-cover"
             alt={card.title}
             src={card.imgSrc}
           />
           <div className="absolute bottom-0 left-0 bg-gradient-to-t from-[#3B2A89] to-transparent w-full h-[8.438rem]" />
-          <div className="w-[85%] group absolute bottom-[62.5px] left-[40px] text-[#903fff] text-lg flex justify-between">
+          <div className="w-[85%] absolute bottom-[62.5px] left-[40px] text-[#903fff] text-lg flex justify-between">
             <b className="font-bold text-5xl">{card.title}</b>
             <img className="group-hover:rotate-90 transition-transform" src="objects1.svg" alt="" srcset="" />
           </div>
